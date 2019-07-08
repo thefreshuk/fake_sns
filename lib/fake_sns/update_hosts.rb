50f3e3887ccd8f7cb8c39a035dc97e1b355545e3
Panamus <7037867+Panamus@users.noreply.github.com>
2019-07-08 12:08:52 +0100

    Fake SNS update hosts
    Co-Authored-By: James W Dunne <me@jameswdunne.com>

--- lib/fake_sns/update_hosts.rb
+++ lib/fake_sns/update_hosts.rb
@@ -3,20 +3,28 @@ Encoding.default_internal = Encoding::UTF_8
 
 require 'socket'
 require 'hosts'
-
+require 'sudo'
 
 def update_hosts
+  Sudo::Wrapper.run do |sudo|
     ip = IPSocket.getaddress(Socket.gethostname)
-
     hosts = Hosts::File.read('/etc/hosts')
 
-    if hosts.elements[-1].name == "sns.aws.local"
-        hosts.elements[-1].address = ip
-    else
-        Hosts::Entry.new(ip, 'sns',
-                         :aliases => ['sns.aws.local'],
-                         :comment => 'Fake SNS local')
+    if_none = lambda do
+      entry = Hosts::Entry.new(ip, 'fakesns',
+                               :aliases => ['sns.aws.local'],
+                               :comment => 'Fake SNS local')
+      hosts.elements << entry
+      entry
     end
 
-    hosts.write
+    element = hosts.elements.find(if_none) do |element|
+      next unless element.respond_to?(:name)
+      element.name == "fakesns"
+    end
+
+    element.address = ip
+
+    sudo[hosts].write
+  end
 end
